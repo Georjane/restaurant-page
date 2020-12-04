30d39fd4a19588a66eb9cbe2c857fabc4f0b10d3
Georjane <pzgeorjane@gmail.com>
2020-12-04 05:32:32 +0100

    Add loader for images in webpack

--- src/home.js
+++ src/home.js
@@ -1,15 +1,35 @@
+const container = document.querySelector('div#content');
+const nav = document.createElement('nav');
+const main = document.createElement('main');
 
-  const nav = document.createElement('nav');
-  const nav1 = document.createElement('nav');
+
 const restaurant = () => {
+
+  const list = document.createElement('ul');
+  const listItem1 = document.createElement('li');
+  
+  const image = document.createElement('img');
+  const description = document.createElement('div');
+  const restoHeading = document.createElement('h1');
+  const restoAbout = document.createElement('p');
+
+  restoHeading.textContent = `It's that tasty!!!`;
+  restoAbout.textContent = `We create a special atmosphere with our charm and humor. And the food is some of the best you can find anywhere! We specialize in local seasonal specialties but with chef innovations.`;
+
+  description.classList = 'description'
+
+  image.src = './restaurant.jpg';
   nav.textContent = 'I am a nav';
-  nav1.textContent = 'I am a nav1';
-  return {nav, nav1};
+  
+  description.appendChild(restoHeading)
+  description.appendChild(restoAbout)
+  main.appendChild(image)
+  main.appendChild(description)
+  return {nav};
 }
 
-const element = document.querySelector('div#content');
-  element.appendChild(nav);
-  element.appendChild(nav1);
+container.appendChild(nav);
+container.appendChild(main);
 
 
 export default restaurant
